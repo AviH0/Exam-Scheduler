bc7410ad0c76ea957581d22ff7d5d4f204b18834
Yair Maryles <yair.maryles@mail.huji.ac.il>
2021-08-15 18:14:36 +0300

    small corrections to CSsolver

---
+++ CSsolver.py
@@ -0,0 +1,158 @@
+from datetime import date, timedelta
+from typing import List, Set, Dict, Tuple, Iterable, Sequence
+
+import solver
+from dataloader import Dataloader
+from objects import Course
+from state import Evaluator, State
+
+MAX_DATE_DIFF = 10  # maximum amount of days between tests that we let courses effect each other
+MAX_PENALTY = 1.0  # maximum penalty allowed between two courses.
+
+
+class CSstate(State):
+
+    def __init__(self, courses_and_dates: Dict[Course, Tuple[date, date]] = None,
+                 course_list: Iterable[Course] = None, date_list: Tuple[Sequence[date], Sequence[date]] = None,
+                 keep_empty: bool = False):
+        super(CSstate, self).__init__(courses_and_dates, course_list, date_list, keep_empty)
+        self.course_list = course_list
+        self.date_list = date_list
+
+    def new_state(self, new_course: Course, new_dates: Tuple[date, date]):
+        assert new_course in self.courses_dict
+        new_courses_and_dates = self.courses_dict[::]
+        new_courses_and_dates[new_course] = new_dates
+        return CSstate(courses_and_dates=new_courses_and_dates, course_list=self.course_list, date_list=self.date_list)
+
+    def is_pos_goal_state(self):
+        """Checks if cur state is a possible placement (every course is included, at least 18 days between moed's)"""
+        for course in self.course_list:
+             if course not in self.courses_dict:
+                 return False
+             if self.courses_dict[course][1] - self.courses_dict[course][0] < 18:  # todo comp timedelta not int
+                 return False
+        return True
+
+
+class CSsolver(solver):
+    """
+        Class of solvers that build tests schedules according to constraint satisfaction backtracking algorithms.
+    """
+    def __init__(self, loader: Dataloader, evaluator: Evaluator):
+        super(CSsolver, self).__init__(loader, evaluator)
+        self.solutions = set()
+
+    def solve(self) -> Set[State]:
+        """
+            Builds all possible schedules matching hard constraints.
+                The hard constraints are: - 21 days in between tests
+                                          - if the penalty between two tests of some two courses on some two dates
+                                            is more then MAX_PENALTY (defined above)
+        """
+        courses_by_points = self._get_top_10({course: course.course_points for course in self.course_list},
+                                             reverse=False)
+        empty_state = CSstate(course_list=self.course_list, date_list=self.dates, keep_empty=True)
+        empty_courses_penalties = {course: 0 for course in self.course_list}
+
+        for course2start_with in courses_by_points:
+            starting_state = empty_state.new_state(new_course=course2start_with, new_dates=(self.moed_a_dates[0],
+                                                                              self.moed_b_dates[0]))
+            self._backtracker(state=starting_state, solutions=set(),
+                              courses_not_included=self._update_course_penalties(course2start_with,
+                                                                                 empty_courses_penalties))
+        return self.solutions
+
+    def _update_course_penalties(self, new_course: Course,
+                                 courses_not_included: Dict[Course, float]) -> Dict[Course, float]:
+        """
+            Updates dictionary of weights of courses not yet scheduled with the new course being scheduled.
+        :param new_course: new course we want to schedule test for
+        :param courses_not_included: courses that their tests have not yet been scheduled.
+        :return: NEW updated dictionary of courses that have not yet been scheduled and their weights to add them to the
+        schedule. (a new dictionary is returned for backtracking purposes)
+        """
+        new_course_dict = dict()
+        for course2compare in courses_not_included:
+            if course2compare == new_course:
+                continue
+            new_course_dict[course2compare] = courses_not_included[course2compare] +\
+                                              self.course_pair_evaluator((new_course, course2compare))
+
+        return new_course_dict
+
+    def _cal_penalty(self, state: CSstate, courses_included: Set[Course], new_course: Course,
+                     new_dates: Tuple[date, date]) -> float:
+        """
+            Calculates the penalty of adding a test of some course on some two dates in the schedule.
+        :param state: Current state with tests already scheduled.
+        :param courses_included: Set of tests that their tests are already scheduled.
+        :param new_course: Course we want to add to schedule.
+        :param new_dates: Dates we want this new course to have tests on.
+        :return: Float symbolizing the strain on the schedule by adding tests of this subject on these dates.
+        """
+        penalty = .0
+        for course2compare in courses_included:
+            for date2compare in state.courses_dict[course2compare]:
+                for new_date in new_dates:
+                    dates_diff = abs(date2compare - new_date) # check both if moedA of one is close to moedB of other
+                    if dates_diff < MAX_DATE_DIFF:
+                        cur_penalty = 1/dates_diff * self.course_pair_evaluator((new_course, course2compare))
+                        penalty += cur_penalty if cur_penalty < MAX_PENALTY else 0
+        return penalty
+
+    def _get_top_10(self, some_dict: dict, reverse=True):
+        """
+            todo: find func that does this
+            Returns top ten keys of dictionary with lowest values.
+        """
+        sorted_dict = sorted(some_dict.items(), key=lambda x: x[1], reverse=reverse)[:10]
+        return [x[0] for x in sorted_dict]
+
+    def _top_ten_dates2check(self, state: CSstate, courses_included: Set[Course],
+                             new_course: Course) -> List[date]:
+        """
+            Finds the ten best dates for a given course to have it's tests on
+        :param state: Current state with tests already scheduled.
+        :param courses_included: Set of tests that their tests are already scheduled.
+        :param new_course: Course we want to add to schedule.
+        """
+        dates_penalties = dict()
+        for date in self.moed_a_dates:
+            dates_penalties[date] = self._cal_penalty(state, courses_included, new_course,
+                                                      (date, date+timedelta(days=21)))
+
+        return self._get_top_10(dates_penalties)
+
+    def _backtracker(self, state: CSstate, solutions: Set[State], courses_included: Set[Course] = None,
+                    courses_not_included: Dict[Course, float] = None):
+        """
+            CSP algorithm. Backtracks through all possible solutions and returns ones that match constraints.
+            General Build:
+                with base case of a given state as a possible goal state (all tests scheduled while
+                                                                          not violating any hard constraints)
+                for given state - for 10 courses (not yet scheduled) with min penalty on 10 dates with min penalty
+                            continue checking
+        :param state: Current state with tests already scheduled.
+        :param solutions: States that match hard constraints and all courses's tests are scheduled.
+        :param courses_included: Set of tests that their tests are already scheduled.
+        :param courses_not_included: courses that their tests have not yet been scheduled.
+
+        """
+        if state.is_pos_goal_state():
+            self.solutions.add(state)
+            return
+        for course2add in self._get_top_10(courses_not_included):
+            new_courses_not_included = self._update_course_penalties(course2add, courses_not_included)
+            courses_included.add(course2add)
+            for date2add in self._top_ten_dates2check(state, courses_included, course2add):
+                self._backtracker(state.new_state(course2add, (date2add, date2add+timedelta(days=21))), solutions,
+                                  courses_included, new_courses_not_included)
+            courses_included.remove(course2add)
+
+
+
+
+
+
+
---
+++ SAsolver.py
@@ -0,0 +1,105 @@
+from datetime import date, timedelta
+from typing import List, Set, Dict, Tuple, Iterable, Sequence
+
+import solver
+from dataloader import Dataloader
+from objects import Course
+from state import Evaluator, State
+from random import sample, choice, uniform
+from math import exp
+
+"""
+    Things left here:
+        -  find faster way to update score then evaluating every time
+        -  dates change
+        -  find best values for T0 and ITERATION_N
+        -  find best reduce function for T 
+                -> currently is linear with 3,000 jumping steps and 7,000 hill descending steps
+ """
+
+
+SWAP_GENERATOR = "SWAP"
+MOVE_ONE_GENERATOR = "MOVE_ONE"
+MOVE_TWO_GENERATOR = "MOVE_TWO"
+GENERATORS = [SWAP_GENERATOR, MOVE_TWO_GENERATOR, MOVE_ONE_GENERATOR]
+DEFAULT_T0 = 3000
+ITERATION_N = 10000
+SUB_GROUP_N = 5
+
+
+class SAstate(State):
+
+    def __init__(self, courses_and_dates: Dict[Course, Tuple[date, date]] = None,
+                 course_list: Iterable[Course] = None, date_list: Tuple[Sequence[date], Sequence[date]] = None):
+        super(SAstate, self).__init__(courses_and_dates, course_list, date_list)
+        self.course_list = courses_and_dates.keys()
+        self.date_list = date_list
+
+    def get_successor(self, sub_group_n: int, generator: str):
+        # TODO: should we update the total score here? it would be faster since we won't have to recalculate everything
+        # TODO: should we make sure we get a possible successor? hard constraints might be too easy to violate,
+        #  giving us very high prob for wrong solutions and a waist of time
+        orig_state = SAstate(courses_and_dates=self.courses_dict)
+        courses2move = sample(self.course_list, sub_group_n)
+        for course in courses2move:
+            if generator == SWAP_GENERATOR:
+                course2swap = choice(self.course_list)
+                dates2save = self.courses_dict[course]
+                self.courses_dict[course] = self.courses_dict[course2swap]
+                self.courses_dict[course2swap] = dates2save
+            elif generator == MOVE_TWO_GENERATOR:
+                direction = choice([-1, 1])
+                dates = self.courses_dict[course]
+                self.courses_dict[course] = (dates[0] + timedelta(days=direction), dates[1] + timedelta(days=direction))
+            elif generator == MOVE_ONE_GENERATOR:
+                direction = choice([-1, 1])
+                moed = choice([0,1])
+                dates = self.courses_dict[course]
+                if moed == 0:
+                    self.courses_dict[course] = (dates[0] + timedelta(days=direction), dates[1])
+                else:
+                    self.courses_dict[course] = (dates[0], dates[1] + timedelta(days=direction))
+        return orig_state
+    # start of 2nd to do of only giving back solutions that match hard constraints
+    # def check_hard_constraints(self, course_a, dates_a, course_b, dates_b) -> float:
+    #     """Checks to make sure that swapping these courses """
+    #     for course2comp in self.course_list:
+    #         if course2comp == course_a or course2comp == course_b:
+    #             continue
+    #
+    #
+    # def swap_generator(self, course):
+    #     for i in range(3):
+    #         course2swap = choice(self.course_list)
+
+
+class SAsolver(solver):
+
+    def __init__(self, loader: Dataloader, evaluator: Evaluator):
+        super(SAsolver, self).__init__(loader, evaluator)
+        self.course_list = loader.get_course_list()
+        self.weights = loader.get_course_pair_weights()
+        self.dates = loader.get_available_dates()
+
+    def solve(self, state: SAstate, T0 = None) -> Set[State]:
+
+        def reduce_T_lin(T: float):
+            T -= 1 if T > 1 else 0.0001
+            return T
+
+        T = T0 if T0 else DEFAULT_T0
+        generator = None
+        for k in range(ITERATION_N):
+            T = reduce_T_lin(T)
+            if k % 1000 == 0:
+                generator = choice(GENERATORS)
+            orig_state = state.get_successor(SUB_GROUP_N, generator)
+            old_pen = self.evaluator(orig_state)
+            new_pen = self.evaluator(state)
+            if new_pen < old_pen:
+                continue
+            elif uniform(0, 1) < exp(-(abs(old_pen - new_pen))/ T):
+                continue
+            state = orig_state
+        pass
+
