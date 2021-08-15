188544e6899b550421e7b0a9dd21bb98adf228a6
Avinoam <avinoam@hershler.com>
2021-08-15 16:18:57 +0300

    update state.py

--- state.py
+++ state.py
@@ -1,15 +1,15 @@
 import random
 from abc import abstractmethod
 from datetime import date
-from typing import Dict, Tuple, Iterable, Sequence
+from typing import Dict, List, Tuple, Iterable, Sequence
 
-from objects import Course
+from objects import *
 
 
 class State:
 
     def __init__(self, courses_and_dates: Dict[Course, Tuple[date, date]] = None,
-                 course_list: Iterable[Course] = None, date_list: Tuple[Sequence[date], Sequence[date]] = None):
+                 course_list: List[Course] = None, date_list: Tuple[Sequence[date], Sequence[date]] = None):
         """
         Initialise a new state. Each state holds a dictionary where the keys are courses and the values are
         a tuple of dates (moed a and moed b).
@@ -21,21 +21,63 @@ class State:
         courses_and_dates is None. default: None
         """
         assert course_list or courses_and_dates
+        self.course_list = course_list
+        self.date_list = date_list
         self.courses_dict: Dict[Course, Tuple[date, date]] = courses_and_dates
         if not courses_and_dates:
-            self.random_initialise(course_list, date_list)
+            self.random_initialise()
 
-    def random_initialise(self, course_list: Iterable[Course], date_list: Tuple[Sequence[date], Sequence[date]]):
+    def random_initialise(self):  # TODO Random is almost always bad - at least enforce 21 days between aleph and bet
         self.courses_dict = dict()
-        for c in course_list:
-            self.courses_dict[c] = random.choice(date_list[0]), random.choice(date_list[1])
+        for c in self.course_list:
+            self.courses_dict[c] = random.choice(self.date_list[0]), random.choice(self.date_list[1])
 
+    def __repr__(self):
+        str_rep = ""
+        for course in self.courses_dict:
+            str_rep += str(course)+ ":   "
+            str_rep += str(self.courses_dict[course][0]) + ",  "
+            str_rep += str(self.courses_dict[course][1]) + "\n"
 
+        return str_rep
+
+    def get_major_schedule_repr(self, major: Major):
+        repr_str = ""
+        for sem in Semester:
+            repr_str += "____Semester No. " + str(sem.value + 1) + "____\n"
+            sem_courses = major.get_sem_courses(sem)
+            hova = []
+            bhova = []
+            bhira = []
+            for course in sem_courses.keys():
+                if sem_courses[course] == CourseType.HOVA:
+                    hova.append(course)
+                if sem_courses[course] == CourseType.BHIRAT_HOVA:
+                    bhova.append(course)
+                if sem_courses[course] == CourseType.BHIRA:
+                    bhira.append(course)
+
+            repr_str += "hova:   \n"
+            for course in hova:
+                dateA, dateB = self.courses_dict[course]
+                repr_str += "      " + course.number + ":  " + str(dateA) + "  " + str(dateB) + "\n"
+
+            repr_str += "bhova:   \n"
+            for course in hova:
+                dateA, dateB = self.courses_dict[course]
+                repr_str += "      " + course.number + ":  " + str(dateA) + "  " + str(dateB) + "\n"
+
+            repr_str += "bhira:   \n"
+            for course in hova:
+                dateA, dateB = self.courses_dict[course]
+                repr_str += "       " + course.number + ":  " + str(dateA) + "  " + str(dateB) + "\n"
+
+        return repr_str
 
 class Evaluator:
 
-    def __init__(self):
-        pass
+    def __init__(self, course_pair_evaluate):
+        self.course_pair_evalutor = course_pair_evaluate
 
     @abstractmethod
     def evaluate(self, state: State) -> float:
@@ -43,3 +85,40 @@ class Evaluator:
 
     def __call__(self, state: State, *args, **kwargs) -> float:
         return self.evaluate(state)
+
+
+class SumEvaluator(Evaluator):
+
+    def __init__(self, course_pair_evaluate):
+        super().__init__(course_pair_evaluate)
+
+    def evaluate(self, state: State) -> float:
+
+        # first check moed A and B distance
+        # for course in state.courses_dict:
+        #     A_date, B_date = state.courses_dict[course]
+        #     if abs((B_date - A_date).days) < 21:
+        #         return 100
+
+        course_list = list(state.courses_dict.keys())
+        sum = 0
+        for i in range(len(course_list)):
+            for j in range(i + 1, len(course_list)):
+                course1, course2 = course_list[i], course_list[j]
+                course1_dateA, course1_dateB = state.courses_dict[course1]
+                course2_dateA, course2_dateB = state.courses_dict[course2]
+
+                time_A, time_B = abs((course1_dateA - course2_dateA).days) + 0.1, abs((course1_dateB - course2_dateB).days) + 0.1
+
+                course_distance = self.course_pair_evalutor(course1, course2)
+                if course_distance == 0:
+                    sum += 0
+                else:
+                    sum += course_distance * (1 / time_A) + course_distance * (1 / time_B)
+        return sum
+
+
+
+
+
+
