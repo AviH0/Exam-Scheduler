858e6a3ab24f26fea6e8b2be1acd6a57960002a0
Yair Maryles <yair.maryles@mail.huji.ac.il>
2021-08-15 18:12:52 +0300

    small corrections to CSsolver

--- exam_scheduler.py
+++ exam_scheduler.py
@@ -3,6 +3,8 @@ from solver import Solver
 from datetime import date
 from state import *
 from genetic_solver import GeneticSolver
+from CSsolver import CSsolver
+from SAsolver import SAsolver
 
 
 # class ExamScheduler:
@@ -15,8 +17,10 @@ from genetic_solver import GeneticSolver
 def main():
     dl = CSVdataloader("data/data.csv", date(2021, 7, 1), date(2021, 8, 1), date(2021,8,20), [])
     evaluator = SumEvaluator(dl.get_course_pair_weights())
-    solver = GeneticSolver(dl, evaluator)
-    sol = solver.solve(1000)
+    # solver = GeneticSolver(dl, evaluator)
+    # sol = solver.solve(1000)
+    solver = CSsolver(dl, evaluator)
+    sol = solver.solve()
     majors_dict = dl.get_majors_dict()
     major = majors_dict["מדמח חד חוגי"]
     print(sol.get_major_schedule_repr(major))
