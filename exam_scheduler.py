54bb9d2977a55ad896470a0e7576462ffbe18f6d
yair.maryles <marylesyair@gmail.com>
2021-08-19 16:59:50 +0300

    making the exam_scheduler more generic

--- exam_scheduler.py
+++ exam_scheduler.py
@@ -3,8 +3,12 @@ from solver import Solver
 from datetime import date
 from state import *
 from genetic_solver import GeneticSolver
+from SAsolver import SAsolver
 
-
+GENETIC_SOL = "genetic"
+SIM_AN_SOL = "simulated_annealing"
+CS_SOL = "constraint_satisfaction"
+CS_SA_SOL = "simulated_annealing_and_constraint_satisfaction"
 
 # class ExamScheduler:
 #
@@ -13,11 +17,31 @@ from genetic_solver import GeneticSolver
 #         pass
 
 
+def run_solver(solver_type: str, dl: CSVdataloader, evaluator: Evaluator) -> Tuple[Solver, State]:
+
+    solver, sol = None, None
+
+    if solver_type == GENETIC_SOL:
+        solver = GeneticSolver(dl, evaluator, YearSemester.SEM_A)
+        sol = solver.solve(1000)
+    elif solver_type == SIM_AN_SOL:
+        solver = SAsolver(dl, evaluator, YearSemester.SEM_A)
+        sol = solver.solve()
+
+    assert solver is not None and sol is not None
+    return solver, sol
+
+
 def main():
-    dl = CSVdataloader("data/data.csv", date(2021, 7, 1), date(2021, 8, 1), date(2021,8,20), [])
+    dl = CSVdataloader(major_data_dir="data/data.csv", startA=date(2021, 7, 1), endA=date(2021, 8, 1),
+                       startB=date(2021, 8, 1), endB=date(2021,8,20), not_allowed=[],
+                       sem_A_courses_data_dir="data/courses_A.csv", sem_B_courses_data_dir="data/courses_B.csv")
     evaluator = SumEvaluator(dl.get_course_pair_weights())
-    solver = GeneticSolver(dl, evaluator, YearSemester.SEM_A)
-    sol = solver.solve(1000)
+
+    # changing the following value changes solvers being used in program.
+    solver_type = SIM_AN_SOL
+
+    solver, sol = run_solver(solver_type=solver_type, dl=dl, evaluator=evaluator)
     majors_dict = dl.get_majors_dict()
     major = majors_dict["מדמח חד חוגי"]
     print(sol.get_major_schedule_repr(major, YearSemester.SEM_A))
